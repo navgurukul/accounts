9d22155c0cb73aa5bf84489e694a6c70d9e45ae1
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-07-18 13:48:46 +0530

    added for both platforms

--- src/components/AleradyLoggedIn.js
+++ src/components/AleradyLoggedIn.js
@@ -1,22 +1,15 @@
 import React, { useEffect, useState } from "react";
-
 function AlreadyLoggedIn() {
   const urlParams = new URL(document.location.href);
   const searchParams = urlParams.searchParams;
   const logout = searchParams.get("logout");
-  let [originUrl, setOriginUrl] = useState(localStorage.getItem("prev"))
-
-  const iframe = document.querySelector("meraki");
-  const iframeScratch = document.querySelector("#scratch");
-
-  const url = "https://meraki-k069df6h5-meraki-dev.vercel.app/"
-  const urlScratch = "https://sso-login.d3laxofjrudx9j.amplifyapp.com"
+  let [originUrl, setOriginUrl] = useState(localStorage.getItem("prev")) 
 
   useEffect(() => {
     const idToken = localStorage.getItem("token");
     let user = JSON.parse(localStorage.getItem("user"));
-    console.log(user, originUrl, "user")
-    setOriginUrl(localStorage.getItem("prev"));
+    console.log(user,  originUrl,"user")
+     setOriginUrl(localStorage.getItem("prev"));
     const message = {
       type: "USER_LOGIN",
       payload: {
@@ -24,23 +17,20 @@ function AlreadyLoggedIn() {
         userDetails: user,
       },
     };
-    const iframeLoadHandler = (url) => {
-      // const iframe = document.querySelector("iframe");
-
+    const iframeLoadHandler = () => {
+      const iframe = document.querySelector("iframe");
       const window = iframe.contentWindow;
-      const scratchWindow = iframeScratch.contentWindow;
-      const targetOrigin = url;
-      const scratchTargetUrl = urlScratch;
+      const targetOrigin = originUrl;
       window.postMessage(message, targetOrigin);
-      scratchWindow.postMessage(message, scratchTargetUrl);
       return true
     };
 
+    const iframe = document.querySelector("iframe");
     iframe.addEventListener("load", iframeLoadHandler);
 
-    setTimeout(() => {
-      window.location.href = `${originUrl}login`
-    }, 6000);
+    // setTimeout(() => {
+    // window.location.href = `${originUrl}login`
+    // }, 6000);
 
 
     return () => {
@@ -51,29 +41,24 @@ function AlreadyLoggedIn() {
       }, 5000);
     };
   }, []);
+  // useEffect(() => {
+  //   const idToken = localStorage.getItem("token");
+  //   const message = {
+  //     type: 'USER_LOGIN',
+  //     payload: { token: idToken },
+  //   };
+  //   const iframe = document.querySelector('iframe');
+  //   const window = iframe.contentWindow;
+  //   const targetOrigin = "https://meraki-k069df6h5-meraki-dev.vercel.app/";
+  //   window.postMessage(message, targetOrigin);
+  // }, []);
 
-  useEffect(() => {
-    const idToken = localStorage.getItem("token");
-    const message = {
-      type: 'USER_LOGIN',
-      payload: { token: idToken },
-    };
-    const iframe = document.querySelector('iframe');
-    const window = iframe.contentWindow;
-    const targetOrigin = originUrl;
 
-    window.postMessage(message, targetOrigin);
-
-  }, []);
   return (
     <div>
       <h1>Already Logged In</h1>
-
-      <iframe style={{ width: "300px", height: "300px" }} id="meraki" src={url} title="Sub"></iframe>
-
-      <iframe style={{ width: "300px", height: "300px" }} id="scratch" src={urlScratch} title="Sub"></iframe>
+      <iframe style={{width:"300px",height:"300px"}} src="https://meraki-k069df6h5-meraki-dev.vercel.app/" title="Sub"></iframe>
     </div>
   );
 }
-
 export default AlreadyLoggedIn;
