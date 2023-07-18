5af1462e77cf8e000250113c14025e24b00511f8
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-07-18 14:40:28 +0530

    back to initial

--- src/components/AleradyLoggedIn.js
+++ src/components/AleradyLoggedIn.js
@@ -24,23 +24,13 @@ function AlreadyLoggedIn() {
       window.postMessage(message, targetOrigin);
       return true
     };
-    const scratchLoadHandler = () => {
-      const urlScratch = "http://localhost:8080";
-      const scratchFrame = document.querySelector("#scratch").contentWindow;
-      const scratchWindow = scratchFrame.contentWindow;
-      const scratchTargetUrl = urlScratch;
-      scratchWindow.postMessage(message, scratchTargetUrl);
-      return true
-    };
 
     const iframe = document.querySelector("iframe");
-    const scratchFrame = document.querySelector("#scratch")
-    scratchFrame.addEventListener('load', scratchLoadHandler)
     iframe.addEventListener("load", iframeLoadHandler);
 
     setTimeout(() => {
     window.location.href = `${originUrl}login`
-    }, 10000);
+    }, 6000);
 
 
     return () => {
@@ -51,25 +41,22 @@ function AlreadyLoggedIn() {
       }, 5000);
     };
   }, []);
-  // useEffect(() => {
-  //   const idToken = localStorage.getItem("token");
-  //   const message = {
-  //     type: 'USER_LOGIN',
-  //     payload: { token: idToken },
-  //   };
-  //   const iframe = document.querySelector('iframe');
-  //   const window = iframe.contentWindow;
-  //   const targetOrigin = "https://meraki-k069df6h5-meraki-dev.vercel.app/";
-  //   window.postMessage(message, targetOrigin);
-  // }, []);
-
-
+  useEffect(() => {
+    const idToken = localStorage.getItem("token");
+    const message = {
+      type: 'USER_LOGIN',
+      payload: { token: idToken },
+    };
+    const iframe = document.querySelector('iframe');
+    const window = iframe.contentWindow;
+    const targetOrigin = originUrl;
+    window.postMessage(message, targetOrigin);
+  }, []);
   return (
     <div>
       <h1>Already Logged In</h1>
-      <iframe style={{width:"300px",height:"300px"}} src="https://meraki-k069df6h5-meraki-dev.vercel.app/" title="Sub"></iframe>
-      
-      <iframe style={{width:"300px",height:"300px"}} id="scratch" src="http://localhost:8080" title="Sub"></iframe>
+
+      <iframe style={{width:"300px",height:"300px"}} src={originUrl} title="Sub"></iframe>
     </div>
   );
 }
