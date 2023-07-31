1ae207b8a24cbe6d045f8dac02db576ddab9d097
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-07-31 11:25:40 +0530

    practicing around

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -20,7 +20,7 @@ function Login() {
 
   useEffect(() => {
     if (count >= 3) {
-      // originUrl == 'https://dashboard-delta-plum.vercel.app/' ? window.location.href = `${originUrl}`: window.location.href = `${originUrl}login`
+      originUrl == 'https://dashboard-delta-plum.vercel.app/' ? window.location.href = `${originUrl}`: window.location.href = `${originUrl}login`
     }
   }, [count]);
 
